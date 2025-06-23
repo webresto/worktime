20b2368bbe8bb3bc146dbae0b0a8c34184e7c5f9
VuXfi <x@42.pub>
2025-06-23 18:34:57 +0700

    Remove TypeScript declaration files and package.json for @webresto/worktime library

--- dist/esm2020/lib/index.mjs
+++
@@ -1,5 +0,0 @@
-export * from './worktime.validator';
-export * from './tz';
-export * from './formatDate';
-export * from './scheduleGenerator';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvbGliL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLGNBQWMsc0JBQXNCLENBQUM7QUFDckMsY0FBYyxNQUFNLENBQUM7QUFDckIsY0FBYyxjQUFjLENBQUM7QUFDN0IsY0FBYyxxQkFBcUIsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gJy4vd29ya3RpbWUudmFsaWRhdG9yJztcbmV4cG9ydCAqIGZyb20gJy4vdHonO1xuZXhwb3J0ICogZnJvbSAnLi9mb3JtYXREYXRlJztcbmV4cG9ydCAqIGZyb20gJy4vc2NoZWR1bGVHZW5lcmF0b3InOyJdfQ==
